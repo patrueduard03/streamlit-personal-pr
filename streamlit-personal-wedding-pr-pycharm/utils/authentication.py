b56760238eec7e2839a611a7611325c7b1f3268f
Gheorghe Patru <gheorghe.patru02@e-uvt.ro>
2024-10-05 00:32:56 +0300

    updates

--- streamlit-personal-wedding-pr-pycharm/utils/authentication.py
+++ streamlit-personal-wedding-pr-pycharm/utils/authentication.py
@@ -3,10 +3,13 @@ import streamlit as st
 
 def authenticate_user(username, password):
     """Authenticates user against credentials stored in secrets."""
-    # Check if the secrets are set correctly
+
+    # Verifică dacă 'auth' este în secrete
     if "auth" not in st.secrets:
         st.error("Secrets not configured properly.")
         return False
+
+    st.write("Secrets available:", st.secrets)  # Adaugă această linie pentru debugging
 
     users = st.secrets["auth"]["users"]
     passwords = st.secrets["auth"]["passwords"]
